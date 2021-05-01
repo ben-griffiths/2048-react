f187c95700834881b21fb1a32fb9b1b8f199f88a
ben <bengriffiths1571@gmail.com>
2021-05-01 16:17:14 +0100

    fix: fixed gist issues

--- src/helpers/gist.js
+++ src/helpers/gist.js
@@ -1,17 +1,16 @@
 import axios from "axios";
+import { randomId } from "./common";
+
+axios.defaults.headers = {
+  Authorization: `token ${atob(process.env.REACT_APP_GIST_TOKEN)}`,
+};
 
 const gistId = process.env.REACT_APP_GIST_ID;
 
-const headers = {
-  Authorization: `token ${process.env.REACT_APP_GIST_TOKEN}`,
-};
-
 export const getGist = () => {
-  return axios.get(`https://api.github.com/gists/${gistId}`);
+  return axios.get(`https://api.github.com/gists/${gistId}?${randomId()}`);
 };
 
 export const updateGist = (data) => {
-  return axios.patch(`https://api.github.com/gists/${gistId}`, data, {
-    headers,
-  });
+  return axios.patch(`https://api.github.com/gists/${gistId}`, data);
 };
